fc0f668e6d73d7e718ef209733e4ee22bb7a2032
Karl Danielsson <karl.danielsson@yh.nackademin.se>
2021-02-09 13:30:08 +0100

    Added class OutOfStock

---
+++ src/OutOfStock.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+
+/**
+ * Created by Karl Danielsson - JAVA 20B
+ * Date: 2021-02-09
+ * Time: 13:28
+ * Project: ShoeWorld
+ */
+public class OutOfStock {
+
+    private int id;
+    private LocalDate date;
+
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+}
