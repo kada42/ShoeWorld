88420e3b19051783c244dd83cf2ecea983b64759
Karl Danielsson <karl.danielsson@yh.nackademin.se>
2021-02-10 14:00:52 +0100

    Added functional query-method, Grade-list and constructor

---
+++ src/DB.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Karl Danielsson - JAVA 20B
+ * Date: 2021-02-09
+ * Time: 13:37
+ * Project: ShoeWorld
+ */
+public class DB {
+
+    public static final String DB_NAME = "guess_the_number_db";
+    public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/guess_the_number_db";
+    public static final String USER_NAME = "testuser";
+    public static final String PASSWORD = "testuser";
+
+}
